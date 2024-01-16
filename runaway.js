c1b5959a84c2ab65fdc48b20484f5b762a3a869f
Tanmay Gupta <tanmay@lawn-128-61-34-212.lawn.gatech.edu>
2024-01-16 04:50:18 -0500

    Basic setup completed

---
+++ runaway.js
@@ -0,0 +1,23 @@
+const card = document.getElementById("card");
+
+const yesButton = document.getElementById("yes");
+const noButton = document.getElementById("no");
+
+const maxWidth = window.innerWidth - (window.innerWidth * 0.05);
+const maxHeight = window.innerHeight - (window.innerHeight * 0.05);
+const buttonWidth = maxWidth * 0.10;
+const buttonHeight = maxHeight * 0.07;
+
+const rangeHeight = maxHeight - buttonHeight
+const rangeWidth = maxWidth - buttonWidth;
+
+card.addEventListener("click", () => {
+    card.classList.toggle("flip")
+});
+
+yesButton.addEventListener("click", () => alert("Yay!! Can't wait to join the team!"));
+
+noButton.addEventListener("mouseover", () => {
+    noButton.style.left = Math.floor(Math.random() * (rangeWidth + 1)) + "px";
+    noButton.style.top = Math.floor(Math.random() * (rangeHeight + 1)) + "px";
+});
